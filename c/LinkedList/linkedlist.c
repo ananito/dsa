d152f60f1aad4fc2d2459236170621a2f0b52849
ananito <anisfhomsi@gmail.com>
2024-04-11 19:13:01 +0300

    Linked List in C completed

--- c/LinkedList/linkedlist.c
+++ c/LinkedList/linkedlist.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct Node {
+  int data;
+  struct Node *next;
+} Node;
 
+typedef struct LinkedList {
+  Node *head;
+} LinkedList;
+
+void InsertList(LinkedList *list, int data) {
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  if (newNode == NULL) {
+    return;
+  }
+  newNode->data = data;
+  newNode->next = NULL;
+
+  if (list->head == NULL) {
+    list->head = newNode;
+    return;
+  } else {
+    Node *current = list->head;
+    while (current->next != NULL) {
+
+      current = current->next;
+    }
+    current->next = newNode;
+    return;
+  }
+}
+
+void DestroyList(LinkedList *list) {
+  Node *current = list->head;
+  Node *next;
+  while (current != NULL) {
+    next = current->next;
+    free(current);
+
+    current = next;
+  }
+  free(list);
+}
+void PrintList(LinkedList *list) {
+  Node *current = list->head;
+  while (current != NULL) {
+    printf("%d->", current->data);
+    current = current->next;
+  }
+  printf("NULL\n");
+}
 int main(void) {
+  LinkedList *list = malloc(sizeof(LinkedList));
+  for (int i = 1; i <= 100; i++) {
+    InsertList(list, i);
+  }
+  PrintList(list);
 
-  printf("Hello, World!\n");
+  DestroyList(list);
   return 1;
 }
